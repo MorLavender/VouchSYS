099dd89ef359f6f0a0bd5b2d59d70f496d6a0f92
Mor Lavender <mor.lavender@gmail.com>
2026-01-12 01:05:50 -0500

    Add EC2 instance with ALB and security group configuration

--- main.tf
+++ main.tf
@@ -96,3 +96,97 @@ data "aws_ami" "aws-linux" {
     values = ["hvm"]
   }
 }
+# ///////////////////////////////
+# EC2 INSTANCE
+# ///////////////////////////////
+resource "aws_instance" "frontend" {
+  ami                    = data.aws_ami.aws-linux.id
+  instance_type          = "t3.micro"
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.sg_frontend.id]
+
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras install nginx1 -y
+    systemctl start nginx
+    systemctl enable nginx
+    echo "<h1>Hello from Terraform via ALB</h1>" > /usr/share/nginx/html/index.html
+  EOF
+
+  tags = {
+    Name = "frontend-ec2"
+  }
+}
+
+
+# ///////////////////////////////
+# ALB Security Group
+# ///////////////////////////////
+resource "aws_security_group" "alb_sg" {
+  name   = "alb_sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+# ///////////////////////////////
+# Load Balancer
+# ///////////////////////////////
+resource "aws_lb" "frontend_alb" {
+  name               = "frontend-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
+}
+# ///////////////////////////////
+# Target Group
+# ///////////////////////////////
+resource "aws_lb_target_group" "frontend_tg" {
+  name     = "frontend-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+# ///////////////////////////////
+# Attach EC2 to Target Group
+# ///////////////////////////////
+resource "aws_lb_target_group_attachment" "frontend_attach" {
+  target_group_arn = aws_lb_target_group.frontend_tg.arn
+  target_id        = aws_instance.frontend.id
+  port             = 80
+}
+# ///////////////////////////////
+# Listener
+# ///////////////////////////////
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.frontend_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
+output "alb_dns_name" {
+  value = aws_lb.frontend_alb.dns_name
+}
+
+
+
+
+
